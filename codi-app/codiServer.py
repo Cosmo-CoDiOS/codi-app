f46215a612b682e68a9a9e5f80be39b77b116d52
Davide Guidi <dguidi@davides-mbp.mynet>
2020-12-26 21:36:02 +0000

    feat: Removed redirected logs to /tmp
    This commit removed the log forced redirection to /tmp.
    Stdout and stderr can now be used as usual.
    
    Signed-off-by: Dom Rodriguez <shymega@shymega.org.uk>

--- codi-app/codiServer.py
+++ codi-app/codiServer.py
@@ -22,16 +22,6 @@ import EventListener
 import Addressbook
 import lock_file
 
-class writer(object):
-    def __init__(self, file):
-        self.log = open(file, 'w+')
-    def write(self, data):
-        self.log.write(data)
-        self.log.flush()
-
-sys.stdout = writer('/tmp/codi.out')
-sys.stderr = writer('/tmp/codi.err')
-
 def signalHandler(_signo, _stack_frame):
     mtkCmd.SetMouse(0, 1)
     mtkCmd.SetCoDiStatus(3, 3, 3)
